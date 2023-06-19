77f53c8139b0152803ea66d36aaea347c2c8986b
sean-alex <53319943+sean-alex@users.noreply.github.com>
2023-06-19 01:51:54 -0700

    Delete FileWave/filewave_client_log_file/error_messages_in_filewave_client_log_file directory

--- FileWave/filewave_client_log_file/error_messages_in_filewave_client_log_file/log_analysis_filewave_client_errors_failed_to_update_inventory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# checks last 50,000 lines of fwcld.log, grep for current day, then check for a FileWave error entry specific to:
-
-currentDate=$(date "+%Y-%m-%d")
-
-tail -n 50000 /var/log/fwcld.log | grep "$currentDate" | grep -c "main|FATAL|CLIENT|Failed to update inventory"
-
-exit 0
